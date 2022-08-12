2c1c0f36b9e3f4cfa0b3c4fce92227e5c2145faa
Carl <ninjapigeon@users.noreply.github.com>
2022-08-11 19:47:11 -0700

    Typos again

--- module/actor/animal-sheet.js
+++ module/actor/animal-sheet.js
@@ -1,11 +1,11 @@
-import { MYZActorSheet } from "./actor-sheet.js";
+import { YZActorSheet } from "./actor-sheet.js";
 
-export class MYZAnimalSheet extends MYZActorSheet {
+export class YZAnimalSheet extends YZActorSheet {
     /** @override */
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
-            classes: ["mutant-year-zero", "sheet", "actor"],
-            template: "systems/mutant-year-zero/templates/actor/animal-sheet.html",
+            classes: ["yearzero", "sheet", "actor"],
+            template: "systems/yearzero/templates/actor/animal-sheet.html",
             width: 720,
             height: 720,
             tabs: [
